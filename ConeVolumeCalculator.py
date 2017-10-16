cb936c8c788c54db200355ba6103913db7999a33
vladdcsbbk <vlad@dcs.bbk.ac.uk>
2017-10-16 15:26:45 +0100

    fixed the slow computation problem

--- ConeVolumeCalculator.py
+++ ConeVolumeCalculator.py
@@ -13,11 +13,13 @@ import time
 # be sure to return an integer
 def calculateConeVolume(r, h):
 
-    time.sleep(10)
+#    time.sleep(10)
     
-    volume = 1/3*math.pi*r**2*h
-    volume = round(volume, 2)   
-    return volume
+#    volume = 1/3*math.pi*r**2*h
+#    volume = round(volume, 2)   
+#    return volume
+
+    return 10
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
