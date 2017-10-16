7b9bdd21b9fd9a050d19ba49fc34419314e5f95f
vlryzhikov <vladislav.ryzhikov@gmail.com>
2017-10-16 11:46:21 +0100

    updated the coneVolumeCalculator functions

--- ConeVolumeCalculator.py
+++ ConeVolumeCalculator.py
@@ -5,17 +5,22 @@
 # Calculator.net (http://www.calculator.net/volume-calculator.html)
 
 import math
+import time
 ## volume = 1/3πr2h
 ## where r is the radius of the base, h is the height 
 
 # Define Function below
 # be sure to return an integer
 def calculateConeVolume(r, h):
+
+    time.sleep(10)
+    
     volume = 1/3*math.pi*r**2*h
     volume = round(volume, 2)   
     return volume
+
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
-    answer = calculateConeVolume(20.0, 10.0)
-    print(answer)
+
+     print(calculateConeVolume(3, 5))
